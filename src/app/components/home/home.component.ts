d72ba5e7aac1c2e752a412fe312a688d673874f8
Will Slaunwhite <williamslaunwhite@gmail.com>
2023-02-08 20:19:40 -0800

    clean up home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  faLinkedin = faLinkedin;
+  faGithub = faGithub;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openPdf(){
+    window.open("assets/Resume.pdf");
   }
 
 }
